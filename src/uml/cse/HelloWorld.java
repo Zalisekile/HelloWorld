bfd19c3352a791d3cd7630881e0d740b10292f81
Zalisekile Coto <zalisky@gmail.com>
2021-08-11 03:20:25 +0200

    Update HelloWorld.java

--- src/uml/cse/HelloWorld.java
+++ src/uml/cse/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hellow World");
 		System.out.println("How are you?");
+		System.out.println("All gravy baby and You?");
 	}
 }
